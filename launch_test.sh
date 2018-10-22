a4ab1de41081d51737e8370203cafc6e3daf3ca5
Rob Jahn <robjahn@gmail.com>
2018-10-22 12:22:49 -0400

    Update launch_test.sh

--- launch_test.sh
+++ launch_test.sh
@@ -8,11 +8,13 @@ showUsage()
   echo "Example:  ./launch_test.sh frontend_load.jmx 35.237.86.214 8079 5 500 frontend_load"
 }
 
+TestScript=/scripts/$1
 if [ -z "$1" ]; then
   echo "Usage: Arg 1 needs to be valid.  Example: yourtestscript.jmx"
   showUsage
   exit 1
 fi
+ServerURL=$2
 if [ -z "$2" ]; then
   echo "Usage: Arg 2 needs to be the URL or IP of your service that should be tested"
   showUsage
@@ -36,12 +38,11 @@ if [ -z "$6" ]; then
 fi
 
 echo "Running test with the following arguments:"
-echo "Script: $1"
-echo "Result folder: $2"
-echo "Server URL: $3"
+echo "Script: $TestScript"
+echo "Server URL: $ServerURL"
 echo "Server Port: $ServerPort"
 echo "VUCount: $VUCount"
 echo "LoopCount: $LoopCount"
 echo "Dynatrace Load Test Name (LTN): $DT_LTN"
 
-docker run --name jmeter-test -v "${PWD}/scripts":/scripts -v "${PWD}/$2":/results -v "${PWD}/results_log":/results_log -v "${PWD}/results_raw":/results_raw --rm -d $DOCKER_IMAGE_NAME ./jmeter/bin/jmeter.sh -n -t /scripts/$1 -f -e -j /results_log/jmeter.log -l /results_raw/result.tlf -JSERVER_URL="$3" -JDT_LTN="$DT_LTN" -JVUCount="$VUCount" -JLoopCount="$LoopCount"
+docker run --name jmeter-test -v "${PWD}/scripts":/scripts -v "${PWD}/$2":/results -v "${PWD}/results_log":/results_log -v "${PWD}/results_raw":/results_raw --rm -d $DOCKER_IMAGE_NAME ./jmeter/bin/jmeter.sh -n -t $TestScript -f -e -j /results_log/jmeter.log -l /results_raw/result.tlf -JSERVER_URL="$ServerURL" -JDT_LTN="$DT_LTN" -JVUCount="$VUCount" -JLoopCount="$LoopCount"
